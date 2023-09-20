ecfcc49f076aca2fa090e8b6eb7da0663eb9b99f
Isaiah Brown <ibrown@fandm.edu>
2023-09-20 19:57:11 -0400

    parse parameters works

--- newshell.c
+++ newshell.c
@@ -4,6 +4,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <string.h>
 
 char *read_command_naive(void)
 {
@@ -25,10 +26,34 @@ char *read_command_naive(void)
         }
         if (pos >= bufsize)
         {
-            bufsize = bufsize * 2;
+            bufsize *= 2;
             buffer = (char *)realloc(buffer, sizeof(char) * bufsize);
         }
     }
+}
+
+char **get_parameters(char* command) 
+{
+    int buffsize = 4;
+    char **parameters = malloc(buffsize * sizeof(char*));
+    
+    char *parameter = strtok(command, " ");
+    int pos = 0;
+
+    while(parameter != NULL) 
+    {
+        parameters[pos] = parameter;
+        parameter = strtok(NULL, " ");
+        pos++;
+
+        if (pos >= buffsize) 
+        {
+            buffsize *= 2;
+            parameters = (char *) realloc(parameters, buffsize * sizeof(char*));
+        }
+    }
+
+    return parameters;
 }
 
 int loopShell()
@@ -48,6 +73,14 @@ int loopShell()
         
         else 
         {
+            char **parameters = get_parameters(command);
+            int pos = 0;
+            while (parameters[pos] != NULL) 
+            {
+                printf("%s\n", parameters[pos]);
+                pos++;
+            }
+            printf("got parameters\n");
             //int s;
             //s = system(command);
             //int s = execve(command);
